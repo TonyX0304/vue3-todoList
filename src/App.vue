e797b00879c112f7c9cdef70052158ad5ba298b1
灵雨 <779658665@qq.com>
2025-07-31 11:11:27 +0800

    element-plus引入

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import { ElButton } from 'element-plus'
+</script>
 
 <template>
   <h1>You did it!</h1>
-  <p>
 
-  </p>
+  <el-button>I am ElButton</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
 </template>
 
 <style scoped></style>
